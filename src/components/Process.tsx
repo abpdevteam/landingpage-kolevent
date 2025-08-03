fd44016fd2bccf691d517a91abcc3fc0af43959b
baowork <99949277+baoquoc29@users.noreply.github.com>
2025-08-03 10:32:23 +0700

    Update video

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -1,13 +1,20 @@
-
+import React from "react";
 
 const Process: React.FC = () => {
-
-
-  return (
-    <section className="bg-white min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-900 justify-center">Video</h1>
-    </section>
-  );
+    return (
+        <section className="w-full flex flex-col items-center justify-center p-0">
+            <video
+                controls
+                className="w-full max-w-none aspect-video shadow-lg"
+            >
+                <source
+                    src="https://vtvgo-vods.vtvdigital.vn/Qx1DmFPi8fWmtZUBY4jhgA/1754206019/vtvgo-media/vod/2025/07/28/KOLKOC.mp4"
+                    type="video/mp4"
+                />
+                Trình duyệt của bạn không hỗ trợ video HTML5.
+            </video>
+        </section>
+    );
 };
 
 export default Process;
