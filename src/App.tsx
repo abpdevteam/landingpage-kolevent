b3ff11273dd3982e2d95485b8cdf0e51f0e33bcd
baowork <99949277+baoquoc29@users.noreply.github.com>
2025-08-07 21:52:08 +0700

    Update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-
-import Services from './components/Services';
-import Process from './components/Process';
-
 import Stats from './components/Stats';
-import Event from "./components/Event.tsx";
-import KOL from "./components/KOL.tsx";
-import DiscussionComponent from "./components/DiscussionComponent.tsx";
-import AllianceIntroduction from "@/components/AllianceIntroduction.tsx";
-import Testimonial from "./components/Testimonial.tsx";
-import Banner from "./components/Banner.tsx";
-import Footer from "./components/Footer.tsx";
-import InfluencerBanner from "@/components/InfluencerBanner.tsx";
-
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Stats />
-      <Services />
-      <Process />
-      <Event />
-        <KOL></KOL>
-        <DiscussionComponent />
-        <AllianceIntroduction></AllianceIntroduction>
-
-        <Testimonial></Testimonial>
-        <InfluencerBanner></InfluencerBanner>
-        <Banner></Banner>
-
-        <Footer />
     </div>
   );
 }
